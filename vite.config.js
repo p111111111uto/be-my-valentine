6717023f35d52e5adc043ec7d33f43090703b460
max <129562629+p111111111uto@users.noreply.github.com>
2025-01-27 17:27:50 -0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base: "/be-my-valentine"
+  base: "/be-my-valentine",
+  plugins: [react()]
 })
